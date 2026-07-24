315047b56d945c767334a24f48cb386cbf36697d
Dmitry Vitsko <dmitryvitsko@yandex.ru>
2026-07-24 20:52:13 +0300

    Refactored the code and addressed the feedback.

--- src/Gender.java
+++ src/Gender.java
@@ -1,5 +1,4 @@
 public enum Gender {
     MALE,
-    FEMALE,
-    OTHER;
+    FEMALE;
 }
--- src/MusicalShow.java
+++ src/MusicalShow.java
@@ -1,12 +1,28 @@
 import java.util.ArrayList;
 
 public class MusicalShow extends Show{
-    MusicAuthor musicAuthor;
-    String librettoText;
+    private MusicAuthor musicAuthor;
+    private String librettoText;
 
     public MusicalShow(String title, double duration, Director director, MusicAuthor musicAuthor, String librettoText) {
         super(title, duration, director);
         this.musicAuthor = musicAuthor;
+        this.librettoText = librettoText;
+    }
+
+    public MusicAuthor getMusicAuthor() {
+        return musicAuthor;
+    }
+
+    public void setMusicAuthor(MusicAuthor musicAuthor) {
+        this.musicAuthor = musicAuthor;
+    }
+
+    public String getLibrettoText() {
+        return librettoText;
+    }
+
+    public void setLibrettoText(String librettoText) {
         this.librettoText = librettoText;
     }
 
